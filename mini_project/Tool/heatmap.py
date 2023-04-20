5545b5f21ad50da537535b7d88cc7416802e0ad4
Everloom-129 <Everloom-129>
2023-04-20 10:20:07 -0500

    can convert graph sucessfully

--- mini_project/Tool/heatmap.py
+++ mini_project/Tool/heatmap.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import Rbf
 
+import seaborn as sns
+
+
+
 def generate_heatmap(data, x_points, y_points):
     x = data['x_coordinate']
     y = data['y_coordinate']
@@ -18,36 +22,74 @@ def generate_heatmap(data, x_points, y_points):
     return X, Y, Z
 
 def main():
-    # Read the preprocessed data CSV file
-    preprocessed_data_file = "preprocessed_data.csv"
-    data = pd.read_csv(preprocessed_data_file)
+    print("generating the heatmap...")
+    create_heatmap("raw_data//raw_data.csv", "floor_plan\Siebel-Floor-Plan-1.jpg", "heatmap\h1.png")
 
-    # Define the dimensions of the floor plan
-    floor_width = 100
-    floor_height = 100
+    # # Read the preprocessed data CSV file
+    # preprocessed_data_file = "preprocessed_data.csv"
+    # data = pd.read_csv(preprocessed_data_file)
 
-    # Define the resolution of the heatmap
-    resolution = 1
+    # # Define the dimensions of the floor plan
+    # floor_width = 100
+    # floor_height = 100
 
-    # Generate a grid of points for the entire floor plan
-    x_points = np.arange(0, floor_width, resolution)
-    y_points = np.arange(0, floor_height, resolution)
+    # # Define the resolution of the heatmap
+    # resolution = 1
 
-    # Generate the heatmap
-    X, Y, Z = generate_heatmap(data, x_points, y_points)
+    # # Generate a grid of points for the entire floor plan
+    # x_points = np.arange(0, floor_width, resolution)
+    # y_points = np.arange(0, floor_height, resolution)
 
-    # Save the heatmap as a numpy array
-    heatmap_file = "heatmap.npy"
-    np.save(heatmap_file, Z)
-    print(f"Heatmap saved to {heatmap_file}")
+    # # Generate the heatmap
+    # X, Y, Z = generate_heatmap(data, x_points, y_points)
 
-    # Plot the heatmap
-    plt.imshow(Z, cmap='coolwarm', origin='lower', extent=[0, floor_width, 0, floor_height], aspect='auto')
-    plt.colorbar(label='Signal Strength')
-    plt.title('Wi-Fi Network Heatmap')
-    plt.xlabel('X Coordinate')
-    plt.ylabel('Y Coordinate')
-    plt.show()
+    # # Save the heatmap as a numpy array
+    # heatmap_file = "heatmap.npy"
+    # np.save(heatmap_file, Z)
+    # print(f"Heatmap saved to {heatmap_file}")
+
+    # # Plot the heatmap
+    # plt.imshow(Z, cmap='coolwarm', origin='lower', extent=[0, floor_width, 0, floor_height], aspect='auto')
+    # plt.colorbar(label='Signal Strength')
+    # plt.title('Wi-Fi Network Heatmap')
+    # plt.xlabel('X Coordinate')
+    # plt.ylabel('Y Coordinate')
+    # plt.show()
+
+
+def create_heatmap(raw_data_path, floor_plan_path, heatmap_path):
+    # Load Wi-Fi data
+    wifi_data = pd.read_csv(raw_data_path)
+
+    # Calculate grid size based on the number of unique latitude and longitude values
+    num_lat = len(wifi_data['Latitude'].unique())
+    num_lon = len(wifi_data['Longitude'].unique())
+
+    # Create an empty grid for the heatmap
+    heatmap_data = np.zeros((num_lat, num_lon))
+
+    # Fill the heatmap grid with signal strength data
+    for index, row in wifi_data.iterrows():
+        lat_index = np.where(wifi_data['Latitude'].unique() == row['Latitude'])[0][0]
+        lon_index = np.where(wifi_data['Longitude'].unique() == row['Longitude'])[0][0]
+        heatmap_data[lat_index, lon_index] = row['Signal Strength']
+
+    # Set up the figure and axis
+    fig, ax = plt.subplots()
+
+    # Display the floor plan image
+    floor_plan = plt.imread(floor_plan_path)
+    ax.imshow(floor_plan, extent=[0, num_lon, 0, num_lat], aspect='auto')
+
+    # Create the heatmap
+    sns.heatmap(heatmap_data, cmap='coolwarm', alpha=0.5, annot=True, fmt='.0f', cbar=False)
+
+    # Save the heatmap to a file
+    plt.savefig(heatmap_path)
+
+# Example usage
+
+
 
 if __name__ == "__main__":
     main()
