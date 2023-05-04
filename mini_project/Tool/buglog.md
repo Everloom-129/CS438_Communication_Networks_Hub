85e82cdd06fde53e3b4d3302b06fad7ad8610f73
Everloom-129 <Everloom-129>
2023-05-03 20:13:31 -0500

    add main notebook

--- mini_project/Tool/buglog.md
+++ mini_project/Tool/buglog.md
@@ -3,9 +3,8 @@ Wang,Jie
 Wu,Jiaxin
 
 ## Data_collection
-1. 
-
-
+To do:
+Mark test locations on the map, then put the coordinates in a csv.
 <--------------------------------------------------------->
                           BUG 1                          
 <--------------------------------------------------------->
@@ -20,10 +19,32 @@ Description:
     - eduroam,44:12:44:6d:cd:91:,-68,-1.9893346,30.0861036
     - eduroam,44:12:44:6d:cd:91:,-68,-1.9893346,30.0861036
 Solution:
-    This is beacuse each AP has two signal spectrum 
+    This is beacuse each AP has two signal spectrum: 2.4G and 5G
+    According to the IT team, they are not implicitly shown in the WLAN
+    but we can still check the protocol our device is using
+    It seems it will automatically select the highest speed one.
+    - Jie's laptop can only support WIFI 5, U/D = 520/650 (Mbps)
+    - Jiaxin's laptop can support WIFI 6, U/D = 880/910 (Mbps)
 
+    We handle this difference in Data_preprocessing.py
 
 ## Data_preprocessing
+To do:
+ 
+<--------------------------------------------------------->
+                          BUG 1                          
+<--------------------------------------------------------->
+Time:
+    2022.05.02.
+Creator:
+    Jiaxin Wu
+Description:
+For each test location, we need also check which mac address 
+we are connecting at that location, and the signal strength of 
+that AP will be kept as the signal strength at this location.
+    
+Solution:
+    
 
 ## Heatmap_generation
 
--- mini_project/Tool/heatmap.py
+++ mini_project/Tool/heatmap.py
@@ -2,15 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import Rbf
-
 import seaborn as sns
-
-
+from PIL import Image
 
 def generate_heatmap(data, x_points, y_points):
-    x = data['x_coordinate']
-    y = data['y_coordinate']
-    signal_strength = data['signal_strength_normalized']
+    x = data['x']
+    y = data['y']
+    signal_strength = data['Signal_Strength']
 
     # Create a grid of points for the entire floor plan
     X, Y = np.meshgrid(x_points, y_points)
@@ -22,12 +20,9 @@ def generate_heatmap(data, x_points, y_points):
     return X, Y, Z
 
 def main():
-    print("generating the heatmap...")
-    # create_heatmap("raw_data.csv", "floor_plan\Siebel-Floor-Plan-1.jpg", "heatmap\h1.png")
-
-    # Read the preprocessed data CSV file
-    preprocessed_data_file = "preprocessed_data.csv"
-    data = pd.read_csv(preprocessed_data_file)
+    # Read the raw data CSV file
+    raw_data_file = "raw_data.csv"
+    data = pd.read_csv(raw_data_file)
 
     # Define the dimensions of the floor plan
     floor_width = 100
@@ -43,54 +38,30 @@ def main():
     # Generate the heatmap
     X, Y, Z = generate_heatmap(data, x_points, y_points)
 
-    # Save the heatmap as a numpy array
-    heatmap_file = "heatmap.npy"
-    np.save(heatmap_file, Z)
-    print(f"Heatmap saved to {heatmap_file}")
+    # Load the floor plan image using PIL (Python Imaging Library)
+    floor_plan_path = "AP_info\F1.png"
+    floor_plan_image = Image.open(floor_plan_path)
+    floor_plan_array = np.array(floor_plan_image)
 
-    # Plot the heatmap
-    plt.imshow(Z, cmap='coolwarm', origin='lower', extent=[0, floor_width, 0, floor_height], aspect='auto')
-    plt.colorbar(label='Signal Strength')
+    # Create a new figure and axis
+    fig, ax = plt.subplots()
+
+    # Display the floor plan image on the axis
+    ax.imshow(floor_plan_array, extent=[0, floor_width, 0, floor_height], aspect='auto')
+
+    # Plot the heatmap on top of the floor plan image
+    heatmap = ax.imshow(Z, cmap='coolwarm', alpha=0.5, origin='lower', extent=[0, floor_width, 0, floor_height], aspect='auto')
+    plt.colorbar(heatmap, label='Signal Strength')
     plt.title('Wi-Fi Network Heatmap')
     plt.xlabel('X Coordinate')
     plt.ylabel('Y Coordinate')
-    plt.show()
 
-
-def create_heatmap(raw_data_path, floor_plan_path, heatmap_path):
-    # Load Wi-Fi data
-    wifi_data = pd.read_csv(raw_data_path)
-
-
-    # Create an empty grid for the heatmap
-    max_x = 10
-    max_y = 10
-    heatmap_data = np.zeros((max_x, max_y))
-
-
-    # Fill the heatmap grid with signal strength data
-    for index, row in wifi_data.iterrows():
-        print(f"Index: {index}, x: {row['x']}, y: {row['y']} ss: {row['Signal Strength']}")
-        x_index = row['x']
-        y_index = row['y']
-        heatmap_data[x_index, y_index] = row['Signal Strength']
-
-    # # Set up the figure and axis
-    # fig, ax = plt.subplots()
-
-    # # Display the floor plan image
-    # floor_plan = plt.imread(floor_plan_path)
-    # ax.imshow(floor_plan, extent=[0, max_x, 0, max_y], aspect='auto')
-    print("heatmap")
-    # Create the heatmap
-    heatmap = sns.heatmap(heatmap_data, cmap='coolwarm', alpha=0.5, annot=True, fmt='.0f', cbar=False)
-    heatmap.get_figure().savefig(heatmap_path)
     # Save the heatmap to a file
-    # plt.savefig(heatmap_path)
+    heatmap_path = "heatmap\heatmap_F1.png"
+    plt.savefig(heatmap_path)
 
-# Example usage
-
-
+    # Show the plot
+    plt.show()
 
 if __name__ == "__main__":
     main()
